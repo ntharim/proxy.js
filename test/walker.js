a6a00438e6d598db1726fc697e2b87e16edfc1fc
Jonathan Ong <jonathanrichardong@gmail.com>
2014-04-15 22:45:13 -0700

    remove all component references

--- test/walker.js
+++ test/walker.js
@@ -34,11 +34,11 @@ describe('Walker', function () {
     it('should have rewritten the JS dependencies', co(function* () {
       var index = tree['https://github.com/component-test/index/0.0.0/index.js'];
       index.uri.should.equal(store + 'github.com/component-test/index/0.0.0/index.js');
-      index.remoteURI.should.equal('https://github.component.io/component-test/index/0.0.0/index.js');
+      index.remoteURI.should.equal('https://github.normalize.us/component-test/index/0.0.0/index.js');
 
       var file = index.file;
       file.string.should.not.include('module stuff from "./stuff.js";');
-      file.string.should.include('module stuff from "https://github.component.io/component-test/index/0.0.0/stuff.js";')
+      file.string.should.include('module stuff from "https://github.normalize.us/component-test/index/0.0.0/stuff.js";')
 
       file = file.dependencies['./stuff.js'].file;
       assert.equal(file.string.trim(), 'export default \'hi\';')
@@ -47,11 +47,11 @@ describe('Walker', function () {
     it('should have rewritten the CSS dependencies', function () {
       var index = tree['https://github.com/component-test/index/0.0.0/index.css'];
       index.uri.should.equal(store + 'github.com/component-test/index/0.0.0/index.css');
-      index.remoteURI.should.equal('https://github.component.io/component-test/index/0.0.0/index.css');
+      index.remoteURI.should.equal('https://github.normalize.us/component-test/index/0.0.0/index.css');
 
       var file = index.file;
       file.string.should.not.include('@import "./something.css";');
-      file.string.should.include('@import "https://github.component.io/component-test/index/0.0.0/something.css";')
+      file.string.should.include('@import "https://github.normalize.us/component-test/index/0.0.0/something.css";')
 
       file = file.dependencies['./something.css'].file;
       assert.equal(file.string.trim(), '* {\n  box-sizing: border-box;\n}');
