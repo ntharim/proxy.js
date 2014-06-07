c145887980882029701ecff5bc97e05481c8d07b
Jonathan Ong <jonathanrichardong@gmail.com>
2014-06-07 15:57:16 -0700

    fix npm's ridiculous case-sensitive package names

--- test/normalize-browserify.js
+++ test/normalize-browserify.js
@@ -8,6 +8,20 @@ var server = require('../app/server')
 var store = require('../config').store
 
 describe('normalize package.json', function () {
+  describe('JSONStream@0.8.2', co(function* () {
+    var res
+
+    after(function () {
+      res.agent.close()
+    })
+
+    it('should download', co(function* () {
+      res = yield* request('/npm/-/JSONStream/0.8.2/index.js')
+      res.statusCode.should.equal(200)
+      res.resume()
+    }))
+  }))
+
   describe('main-loop@1.8.0', co(function* () {
     var res
 
